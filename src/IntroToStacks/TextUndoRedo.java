2e73e27dbd2e8457e4c9901b661ec7bf20eeb68f
League <rhymeinparentheses@gmail.com>
2017-09-28 16:55:24 -0700

    20170928

--- src/IntroToStacks/TextUndoRedo.java
+++ src/IntroToStacks/TextUndoRedo.java
@@ -1,6 +1,16 @@
 package IntroToStacks;
 
-public class TextUndoRedo {
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Stack;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class TextUndoRedo implements KeyListener{
 	/* 
 	 * Create a JFrame with a JPanel and a JLabel.
 	 * 
@@ -13,5 +23,72 @@ public class TextUndoRedo {
 	 * off the Stack and added back to the JLabel.
 	 * 
 	 * */
+	
+	JFrame frame;
+	JPanel panel;
+	JLabel label;
+	Stack<String> stack;
+	String string = "";
+	String character;
+	
+	public TextUndoRedo() {
+		setupGUI();
+	}
+	
+	private void setupGUI() {
+		stack = new Stack<String>();
+		frame = new JFrame("text");
+		panel = new JPanel();
+		label = new JLabel();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BorderLayout());
+		panel.setPreferredSize(new Dimension(420,420));
+		label.setText(string);
+		panel.setOpaque(true);
+		label.setOpaque(true);
+		panel.add(label);
+		frame.getContentPane().add(panel);
+		frame.add(panel);
+		frame.pack();
+		frame.setVisible(true);
+		label.setFocusable(true);
+		label.addKeyListener(this);
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		character = String.valueOf(e.getKeyChar());
+		string += character;
+		label.setText(string);
+		frame.pack();
+	}
+	
+	
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
+			stack.push(character);
+			if (string != null && string.length() > 0) {
+		        string = string.substring(0, string.length() - 1);
+		    }
+			label.setText(string);
+			frame.pack();
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_SHIFT) {
+			string += stack.pop();
+			label.setText(string);
+			frame.pack();
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 }
---
+++ src/IntroToStacks/TextUndoRedoRunner.java
@@ -0,0 +1,9 @@
+package IntroToStacks;
+
+public class TextUndoRedoRunner {
+
+	public static void main(String[] args) {
+		TextUndoRedo lol = new TextUndoRedo();
+	}
+
+}
